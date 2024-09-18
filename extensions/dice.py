11fe28bcfaa42854f2f331a0c051e000b281ce93
KroMiose <li_xiangff@163.com>
2024-09-18 21:00:36 +0800

    improve dice extension

--- extensions/dice.py
+++ extensions/dice.py
@@ -21,7 +21,7 @@ __meta__ = ExtMetaData(
 async def dice_roll(event_name: str, description: str, difficulty: int, _ctx: AgentCtx) -> str:
     """对可能产生不同结果的事件发起掷骰检定请求以确认执行结果 (use lang: zh-CN)
 
-    **应用场景: 战斗、防护、反抗、逃跑、随机事件、行为等 (!你需要在这些场景中积极使用掷骰检定!)**
+    **应用场景: 战斗、施法、防护、反抗、逃跑、随机事件、行为、交互等 (!!!你需要在这些场景中积极使用掷骰检定!!!)**
 
     Args:
         event_name (str): 事件名称
@@ -31,24 +31,36 @@ async def dice_roll(event_name: str, description: str, difficulty: int, _ctx: Ag
 
     assert 1 <= difficulty <= 20, "Difficulty should be between 1 and 20"
 
-    def get_result_str(roll_result: int, difficulty: int) -> str:
-        if roll_result == 1 and roll_result < difficulty:
+    # 由于 LLM 倾向指定难度值偏高，因此在指定高难度时随机降低难度
+    if difficulty > 15 and random.randint(1, 20) < 12:
+        add_coin = random.randint(1, 3)
+        fix_diff_show = f"(+{add_coin})"
+        fix_str = f"(幸运祝福: 骰点 +{add_coin})\n"
+        fix_str_en = f"(Lucky bonus: +{add_coin})"
+    else:
+        fix_diff_show = ""
+        add_coin = 0
+        fix_str = ""
+        fix_str_en = ""
+
+    def get_result_str(roll_result: int, fixed_roll_result: int, difficulty: int) -> str:
+        if fixed_roll_result == 1 and fixed_roll_result < difficulty:
             return "大失败!"
-        if roll_result == 20 and roll_result >= difficulty:
+        if roll_result == 20 and fixed_roll_result >= difficulty:
             return "大成功!"
-        if roll_result < difficulty:
+        if fixed_roll_result < difficulty:
             return "失败"
-        if roll_result >= difficulty:
+        if fixed_roll_result >= difficulty:
             return "成功"
         raise ValueError("Invalid roll result")
 
     roll_result = random.randint(1, 20)
-    result_str = get_result_str(roll_result, difficulty)
+    result_str = get_result_str(roll_result, roll_result + add_coin, difficulty)
     await chat_service.send_message(
         _ctx.from_chat_key,
-        f"【检定事件】{event_name} ({difficulty}/20)\n> {description}\n========\n掷骰结果：{result_str} 【{roll_result}】",
+        f"【检定事件】{event_name} ({difficulty}/20)\n> {description}\n========\n{fix_str}掷骰结果：{roll_result}{fix_diff_show} 【{result_str}】",
     )
     return (
-        f"[{event_name}] ({difficulty}/20) roll result: {roll_result}【{result_str}】\n"
+        f"[{event_name}] ({difficulty}/20) {fix_str_en} roll result: {roll_result}{fix_diff_show}【{result_str}】\n"
         "Please continue to generate responses based on the results"
     )
