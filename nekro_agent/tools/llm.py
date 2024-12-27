0e6b4974e8752d7fdd202a60288e19832113d630
KroMiose <li_xiangff@163.com>
2024-12-28 00:40:05 +0800

    improve logger output

--- nekro_agent/tools/llm.py
+++ nekro_agent/tools/llm.py
@@ -1,16 +1,19 @@
 from typing import Dict, List
 
+from nekro_agent.core.config import config
+from nekro_agent.core.logger import logger
 from nekro_agent.libs.miose_llm.clients.chat_openai import (
     gen_openai_chat_response,
     set_openai_base_url,
 )
 
-from nekro_agent.core.config import config
-from nekro_agent.core.logger import logger
-
 
 async def get_chat_response(messages: List[Dict[str, str]], model_group: str = "") -> str:
     model_group = model_group or config.USE_MODEL_GROUP
+    if model_group not in config.MODEL_GROUPS:
+        logger.error(f"指定的模型组 {model_group} 不存在，请检查配置文件")
+        raise ValueError(f"指定的模型组 {model_group} 不存在，请检查配置文件")
+
     for _i in range(3):
         try:
             set_openai_base_url(config.MODEL_GROUPS[model_group]["BASE_URL"])
