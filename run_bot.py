23dfff86962e087c2400c9038f8afa3ff81fd970
KroMiose <li_xiangff@163.com>
2025-01-06 17:36:06 +0800

    fix: workflow

--- run_bot.py
+++ run_bot.py
@@ -1,17 +1,30 @@
-import nonebot
-from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+try:
+    import nonebot
+    from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
-nonebot.init()
+    nonebot.init()
 
-driver = nonebot.get_driver()
-driver.register_adapter(ONEBOT_V11Adapter)
+    driver = nonebot.get_driver()
+    driver.register_adapter(ONEBOT_V11Adapter)
 
+    nonebot.load_from_toml("pyproject.toml")
+except Exception as e:
+    import traceback
 
-nonebot.load_from_toml("pyproject.toml")
+    traceback.print_exc()
+    print(f"Nonebot Init Error: {e}")
+    raise
 
 
 def main():
-    nonebot.run(host="0.0.0.0", port=8021)
+    try:
+        nonebot.run(host="0.0.0.0", port=8021)
+    except Exception as e:
+        import traceback
+
+        traceback.print_exc()
+        print(f"Plugin Load Test Error: {e}")
+        raise
 
 
 if __name__ == "__main__":
