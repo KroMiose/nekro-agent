461a1b7fa4b81611b7006d85cc62f8ac4d957f13
Zaxpris <zaxpris@qq.com>
2025-04-10 22:38:28 +0800

    feat:移除memory插件，将在之后上架插件市场

--- nekro_agent/api/core.py
+++ nekro_agent/api/core.py
@@ -6,12 +6,13 @@
 from nekro_agent.core import logger
 from nekro_agent.core.bot import get_bot
 from nekro_agent.core.config import ModelConfigGroup, config
-from nekro_agent.core.vector_db import get_qdrant_client
+from nekro_agent.core.vector_db import get_qdrant_client, get_qdrant_config
 
 __all__ = [
     "ModelConfigGroup",
     "config",
     "get_bot",
     "get_qdrant_client",
+    "get_qdrant_config",
     "logger",
 ]
